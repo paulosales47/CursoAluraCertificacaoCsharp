649ee5e1562f925b818f9183a91fa7bacf4b172a
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-06-21 18:50:29 -0300

    Locais e Retornos de Ref

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -8,8 +8,7 @@ namespace Aula01
         {
             Console.ForegroundColor = ConsoleColor.Green;
 
-            Out();
-
+            Ref();
 
             Console.ReadKey();
         }
@@ -22,6 +21,31 @@ namespace Aula01
             Console.WriteLine(numeroConvertido);
             Barra();
         }
+
+        public static void Ref()
+        {
+            int[] numeros = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ref int numero = ref RetornarReferenciaIndice(6, numeros);
+            numero = -6;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+
+        public static ref int RetornarReferenciaIndice(int valorProcurado, int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == valorProcurado)
+                    return ref numeros[i];
+            }
+
+            throw new IndexOutOfRangeException("Valor não encontrado no array");
+        }
+
+
 
         public static void Barra()
         {
