3bff30c49c212cc25536aaf961e9225a5b3a2e5b
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-06-21 18:41:36 -0300

    Variáveis Out

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -6,8 +6,26 @@ namespace Aula01
     {
         static void Main(string[] args)
         {
-            
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Out();
+
+
+            Console.ReadKey();
         }
 
+        public static void Out()
+        {
+            Barra();
+            string numero = "512";
+            int numeroConvertido = int.TryParse(numero, out int numeroOut) ? numeroOut : 0;
+            Console.WriteLine(numeroConvertido);
+            Barra();
+        }
+
+        public static void Barra()
+        {
+            Console.WriteLine("============================================================================");
+        }
     }
 }
