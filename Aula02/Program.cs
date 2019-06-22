96cbcd9cf572548f102f24d0434984bbe66499f6
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-06-22 11:16:45 -0300

    Tuplas

---
+++ Aula02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Tupla();
+
+            Console.ReadKey();
+        }
+
+        public static void Tupla()
+        {
+            Barra();
+            Tuple<int, string, double> tupleAntigo = RetornarPedidosAntigo();
+            Console.WriteLine($"{tupleAntigo.Item1} - {tupleAntigo.Item2} - {tupleAntigo.Item3}");
+
+            var (numeroPedido, nomeCliente, valorPedido) = RetornarPedidosNovo();
+            Console.WriteLine($"{numeroPedido} - {nomeCliente} - {valorPedido}");
+            Barra();
+        }
+
+        public static Tuple<int, string, double> RetornarPedidosAntigo()
+        {
+            return new Tuple<int, string, double>(1, "Paulo", 15.95);
+        }
+
+        public static(int numeroPedido, string NomeCliente, double valorPedido) RetornarPedidosNovo()
+        {
+            return (1, "Paulo", 15.95);
+        }
+
+        public static void Barra()
+        {
+            Console.WriteLine("============================================================================");
+        }
+    }
+}
