4306570cd45d24d718cefee52a09bfb41f5e70cf
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-06-23 09:05:28 -0300

    Aprimoramento em literais numéricos

---
+++ Aula04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            LiteraisNumericos();
+            LiteraisNumericos2();
+
+        }
+
+        public static void LiteraisNumericos()
+        {
+            var numero1 = 0b0001;
+            var numero2 = 0b0010;
+            var numero4 = 0b0100;
+            var numero8 = 0b1000;
+
+            Console.WriteLine(numero1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero4);
+            Console.WriteLine(numero8);
+        }
+
+        public static void LiteraisNumericos2()
+        {
+            const double AVOGRADO = 6.022140857747474E23;
+            const double AVOGRADO_NEW = 6.022_140_857_747_474E23;
+
+            Console.WriteLine(AVOGRADO);
+            Console.WriteLine(AVOGRADO_NEW);
+        }
+    }
+}
